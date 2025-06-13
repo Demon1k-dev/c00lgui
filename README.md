72fd31f605ea9c1769a16e016da442d99e71a665
Demonik 3 <michelle_the_dead_cat@mail.ru>
2025-06-13 21:30:15 +0500

    c00lgui_concept

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # c00lgui
+Why c00lgui?
+Because i like c00lkidd..
+
+What does it do?
+Nothing for now.. Planning to make it redirect people to useful software for PC
+
+Do not take this seriously.
---
+++ src/c00lgui_concept.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define C00L_ERROR_PREVENTION 99
+
+void c00lmenu_print();
+void c00lfunctions_main();
+
+int main() { 
+    int choice;
+    c00lmenu_print();
+    if (scanf("%d", &choice) != 1 && getchar() != '\n') {
+        choice = C00L_ERROR_PREVENTION;
+    }
+    c00lfunctions_main(choice);
+    return 0;
+}
+
+void c00lmenu_print() {
+    printf("        /---------------------------\\\n");
+    printf("        |                           |\n");
+    printf("        |        ||       ||        |\n");
+    printf("        |        ||       ||        |\n");
+    printf("        |        ||       ||        |\n");
+    printf("        |                           |\n");
+    printf("        |    ||                ||   |\n");
+    printf("        |     ||              ||    |\n");
+    printf("        |       ==============      |\n");
+    printf("        |                           |\n");
+    printf("        \\---------------------------/\n\n");
+
+    printf("Choose what you want.\n\n");
+    printf("1. PLACEHOLDER CHOICE\n");
+    printf("2. PLACEHOLDER CHOICE\n");
+    printf("3. PLACEHOLDER CHOICE\n");
+    printf("4. PLACEHOLDER CHOICE\n");
+    printf("99. Exit..\n\n");
+    printf("Choice: ");
+}
+
+void c00lfunctions_main(int choice) {
+    switch (choice) {
+        case 1: 
+        printf("First func\n"); // code here
+        break;
+        case 2: 
+        printf("Second func\n"); // code here
+        break;
+        case 3: 
+        printf("Third func\n"); // code here
+        break;
+        case 4: 
+        printf("Fourth func\n"); // code here
+        break;
+        case 99: printf("Oh noes! D:\n"); break;
+    }
+}
